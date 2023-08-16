a155c61b6eb8059e5a330287979d267a0187c66a
Oleg Korneev <olvikorneev@gmail.com>
2023-08-16 16:38:43 +0300

    Completes all tasts of the module

---
+++ Module_04/mod4-app/resources/views/layouts/header.blade.php
@@ -0,0 +1,3 @@
+<a href="#">
+    <p class="logo">LOGO</p>
+</a>
--- Module_04/mod4-app/routes/web.php
+++ Module_04/mod4-app/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('home', ['name' => 'Ivan', 'age' => 3, 'position' => 'admin', 'address' => 'Moscow city',]);
+});
+
+Route::get('/contacts', function () {
+    $data = ['address' => 'Tula', 'post_code' => 300001, 'email' => 'info@tyla.ru', 'phone' => '+79999999999',];
+    return view('contacts', ['data' => $data]);
+});
+
