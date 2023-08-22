0a82ccbdde634a18807761d2611d3e03a5854cf8
Oleg Korneev <olvikorneev@gmail.com>
2023-08-22 18:38:23 +0300

    Created the PdfGeneratorController and UserController. Wrote routes

--- Module_07/mod7-app/routes/web.php
+++ Module_07/mod7-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PdfGeneratorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', [UserController::class, 'index'])->name('showUsers');
+Route::get('/user/{id}', [UserController::class, 'get'])->name('getUser');
+Route::post('/store-user', [UserController::class, 'store'])->name('storeUser');
+Route::get('/resume', [PdfGeneratorController::class, 'index'])->name('createResume');
+
