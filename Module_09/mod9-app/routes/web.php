287aab6fbbb177ab35379d3ecdba25990865bb6f
Oleg Korneev <olvikorneev@gmail.com>
2023-08-27 19:11:59 +0300

    Routers have been created

--- Module_09/mod9-app/routes/web.php
+++ Module_09/mod9-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\NewsHidden;
+use App\Models\News;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('news/create-test', function () {
+    $news = new News();
+
+    $news->title = 'Test news title';
+    $news->body = 'Test news body';
+
+    $news->save();
+    return $news;
+});
+
+Route::get('news/{id}/hide', function ($id) {
+    $news = News::findOrFail($id);
+    $news->hidden = true;
+    $news->save();
+
+    NewsHidden::dispatch($news);
+
+    return 'News hidden';
+})
+
