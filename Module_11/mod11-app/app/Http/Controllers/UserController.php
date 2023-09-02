365d9155c5279f0f89810c7d70cf9eed9d9a3b65
Oleg Korneev <olvikorneev@gmail.com>
2023-09-02 19:48:34 +0300

    The policy has been registered in the AuthServiceProvider and the UserController has been changed

--- Module_11/mod11-app/app/Http/Controllers/UserController.php
+++ Module_11/mod11-app/app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index() {
+        $this->authorize('view-any', User::class);
         return User::all();
     }
 }
