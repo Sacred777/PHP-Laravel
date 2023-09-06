6fd8f9abfdd294c582f812a2390b62a1d03a6584
Oleg Korneev <olvikorneev@gmail.com>
2023-09-06 13:54:07 +0300

    Fixed migratin

--- Module_13/mod13-app/database/migrations/2023_09_06_104718_create_products_table.php
+++ Module_13/mod13-app/database/migrations/2023_09_06_104718_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('sku'),
-            $table->string('name'),
+            $table->string('sku');
+            $table->string('name');
             $table->decimal('price', 9, 3);
             $table->timestamps();
         });
