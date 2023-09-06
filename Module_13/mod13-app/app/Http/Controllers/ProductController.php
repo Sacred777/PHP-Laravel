3cfbdf2de5c7328496915c5473d7d5ca55759bbf
Oleg Korneev <olvikorneev@gmail.com>
2023-09-06 15:53:19 +0300

    Wrote the code in the ProductController

--- Module_13/mod13-app/app/Http/Controllers/ProductController.php
+++ Module_13/mod13-app/app/Http/Controllers/ProductController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function index(): LengthAwarePaginator
+    {
+        return Product::paginate(10);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $product = Product::create([
+            'sku' => $request->get('sku'),
+            'name' => $request->get('name'),
+            'price' => $request->get('price'),
+        ]);
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * @param Product $product
+     * @return Product
+     */
+    public function show(Product $product): Product
+    {
+        return $product;
+    }
+
+    /**
+     * @param Request $request
+     * @param Product $product
+     * @return JsonResponse
+     */
+    public function update(Request $request, Product $product): JsonResponse
+    {
+        $product->update($request->all());
+        return response()->json($product, 202);
+    }
+
+    /**
+     * @param Product $product
+     * @return JsonResponse
+     */
+    public function destroy(Product $product): JsonResponse
+    {
+        $product->delete();
+        return response()->json([], 204);
+    }
 }
